e7bb41480d5cc8413cb79f50d84aa35a7021a4c4
LW068 <ricardocoronaberron@gmail.com>
2023-01-18 01:04:57 +0000

    [Fixed]: task 3

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    return (set_1 & set_2)
+"""Simple Square Project, not over documented"""
+
+
+class Square:
+    """Class representing a simple square"""
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if (not isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __init__(self, _size=0):
+
+        self.size = _size
+
+    def area(self):
+        return self.__size ** 2
