1a82a110216c0c61dd888287302b152595e4d269
LW068 <ricardocoronaberron@gmail.com>
2023-01-18 01:00:03 +0000

    [Fixed]: task 0

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 class Square:
-    """Class Square that defines a square.
+    """Class Square that defines a square object
     """
-    def __init__(self, _size):
-        """Initialize a simple Square
+    def __init__(self, size):
+        """Initialize method that stores the size of the square
         Args:
-            _size (int): Size of the Square.
+            param1 (int): size of the square
         """
-        self.__size = _size
+        self.__size = size
