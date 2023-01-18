974cf5f8ddfbf0fbd3e26085c0347a99f8c67815
LW068 <ricardocoronaberron@gmail.com>
2023-01-18 01:02:33 +0000

    [Fixed]: task 0

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+"""Simple Square Project
+This Module contains a class representing a simple Square.
+Example:
+    >>>new_square = square(2)
+"""
+
+
 class Square:
-    """Class Square that defines a square object
+    """Class representing a simple square
     """
-    def __init__(self, size):
-        """Initialize method that stores the size of the square
+    def __init__(self, _size):
+        """Initialize a simple Square
         Args:
-            param1 (int): size of the square
+            _size (int): Size of the Square.
         """
-        self.__size = size
+        self.__size = _size
