6c80ab976c9191e8313fc403a801c62f160c509f
LW068 <ricardocoronaberron@gmail.com>
2023-01-24 19:18:17 -0800

    [Fixed] wrong question file once more.

--- 0x06-python-more_classes/2-rectangle.py
+++ 0x06-python-more_classes/2-rectangle.py
@@ -7,6 +7,8 @@ This module is composed by a class that defines a Rectangle
 class Rectangle:
     """ Class that defines a rectangle """
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ Method that initializes the instance
         Args:
@@ -16,6 +18,7 @@ class Rectangle:
 
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -114,4 +117,5 @@ class Rectangle:
         """ Method that prints a message when the instance is deleted
         """
 
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
