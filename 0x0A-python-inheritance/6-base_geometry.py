dcb9370126779c170239ee1ddf9e69ab242b4a44
LW068 <ricardocoronaberron@gmail.com>
2023-02-01 01:28:00 +0000

    [Fix]: BaseGeometry class fix.

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+This is the BaseGeometry class
+"""
+
+
 class BaseGeometry:
-    """ Empty class """
+    """ declaring empty class """
+    pass
+
     def area(self):
         raise Exception("area() is not implemented")
