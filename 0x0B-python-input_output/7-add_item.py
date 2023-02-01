b624a556892801334f154871e7eac2f3992144a7
LW068 <ricardocoronaberron@gmail.com>
2023-02-01 03:58:12 +0000

    [Fix]: file 7 not checking.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,21 +6,19 @@ to a Python list, and then save them to a file
 """
 
 
-import sys
-import json
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
 
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+def add_item(items=[]):
+    try:
+        the_list = load("add_item.json")
+    except IOError:
+        the_list = []
+    for item in items:
+        the_list.append(item)
+    save(the_list, "add_item.json")
 
-
-filename = "add_item.json"
-try:
-    items = load_from_json_file(filename)
-except FileNotFoundError:
-    items = []
-
-for arg in sys.argv[1:]:
-    items.append(arg)
-
-save_to_json_file(items, filename)
+if __name__ == "__main__":
+    import sys
+    add_item(sys.argv[1:])
