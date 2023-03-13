a33c735a813ad2822ef9ca4f5633f9f6e77260b7
LW068 <105394574+LW068@users.noreply.github.com>
2023-03-13 16:03:49 -0500

    Update 1-hbtn_header.py

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -6,8 +6,7 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
+    with urllib.request.urlopen(url) as response:
         header = response.info()
         x_request_id = header.get('X-Request-Id')
         print(x_request_id)
